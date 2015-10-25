7c3a31c688afc2ce38ca082112f2d58544ad6e6d
kobrien15 <kobrien15@gmail.com>
2015-10-25 17:33:55 -0500

    test1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hola 5</div>
+                <div class="title">Hola Again 5</div>
             </div>
         </div>
     </body>
